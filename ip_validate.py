855fc5335db3bfa96b60debabb2c6b9acf8a0158
Bilgin OZTURK <bilgin.ozturk@globalmaksimum.com>
2022-03-15 02:15:26 +0300

    optimize ip_valide

--- ip_validate.py
+++ ip_validate.py
@@ -1,3 +1,11 @@
-with open('node_ip', 'r') as f:
-    node_ip=f.read()
-    print(node_ip)
+import os.path
+
+filename = "node_ip"
+
+if os.path.isfile(filename):
+
+    with open(filename, 'r') as file:
+        print(file.read())
+
+else:
+    print(f"file {filename} does not exist")
